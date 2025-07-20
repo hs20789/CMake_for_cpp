4cda5c9e9ebaa93641557f13812b4ada5b83871b
HeonSu <hs20789@outlook.com>
2025-07-21 08:14:56 +0900

    First Commit

---
+++ Project1/src/Project1.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+int main () {
+    // 표준 __cplusplus: 모든 컴파일러에서 표준값. 단, Visual Studio는 옵션에 따라 199711L만 반환될 수 있음.
+    std::cout << "__cplusplus: " << __cplusplus << std::endl;
+
+#ifdef _MSVC_LANG
+    // Visual Studio 전용: 진짜 활성화된 표준 버전
+    std::cout << "_MSVC_LANG: " << _MSVC_LANG << std::endl;
+#endif
+
+    return 0;
+}
