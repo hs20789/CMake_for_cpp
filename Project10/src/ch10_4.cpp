c30f88a2ed56ffeb5e31ee6b9d91e4b9ec472c5d
HeonSu <hs20789@outlook.com>
2025-08-12 20:14:39 +0900

    Multiple Inherit2

---
+++ Project10/src/ch10_4.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+class Animal
+{
+  public:
+    virtual void eat() = 0;
+};
+
+class Dog : public Animal
+{
+  public:
+    virtual void bark() { std::cout << "Woof!" << std::endl; }
+    void eat() override { std::cout << "The dog ate." << std::endl; }
+};
+
+class Bird : public Animal
+{
+  public:
+    virtual void chirp() { std::cout << "Chirp!" << std::endl; }
+    void eat() override { std::cout << "The bird ate." << std::endl; }
+};
+
+class DogBird : public Dog, public Bird
+{
+  public:
+    using Dog::eat;
+};
+
+int main() {}
