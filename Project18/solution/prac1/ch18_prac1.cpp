edb883262c6f8f185c8eaa5473ed51cbb25dce88
HeonSu <hs20789@outlook.com>
2025-08-25 12:55:42 +0900

    🧑‍💻 ch18_Prac1 🧑‍💻

---
+++ Project18/solution/prac1/ch18_prac1.cpp
@@ -0,0 +1,33 @@
+﻿#include <format>
+#include <iostream>
+#include <iterator>
+#include <vector>
+
+void printVector(std::vector<int> vec)
+{
+    for (auto const &v : vec) {
+        std::cout << v << " | ";
+    }
+    std::cout << std::endl;
+}
+
+int main()
+{
+    std::vector values{2, 5};
+    printVector(values);
+    int inputArr[]{3, 4};
+    values.insert(std::cbegin(values) + 1, std::begin(inputArr),
+                    std::end(inputArr));
+    printVector(values);
+
+    std::vector myVector{0, 1};
+    values.insert(std::cbegin(values), std::begin(myVector),
+                  std::end(myVector));
+    printVector(values);
+
+    std::vector<int> test;
+    for (auto iter{std::crbegin(values)}; iter != std::crend(values); ++iter) {
+        test.emplace_back(*iter);
+    }
+    printVector(test);
+}
