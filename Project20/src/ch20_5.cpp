53087ecf2bca3c23a3476fcffa1a71dc6e3a4dd0
HeonSu <hs20789@outlook.com>
2025-08-27 20:08:41 +0900

    🧑‍💻: std::transform()

--- Project20/src/ch20_5.cpp
+++ Project20/src/ch20_5.cpp
@@ -2,6 +2,30 @@
 #include <iostream>
 #include <vector>
 
+void populateContainer(auto &cont)
+    requires requires(decltype(cont) c, int v) {
+        { c.push_back(v) } -> std::same_as<void>;
+    }
+{
+    while (true) {
+        std::cout << "Enter a number (0 to quit): ";
+        int value;
+        std::cin >> value;
+        if (value == 0) {
+            break;
+        }
+        cont.push_back(value);
+    }
+}
+
+void printContainer(auto &cont)
+{
+    for (auto const &v : cont) {
+        std::cout << v << " || ";
+    }
+    std::cout << std::endl;
+}
+
 int main()
 {
     std::vector<int> values(10);
@@ -16,4 +40,18 @@ int main()
     }
     std::cout << std::endl;
     std::cout << "value : " << value << '\n';
+
+    std::vector<int> myVector;
+    populateContainer(myVector);
+
+    std::cout << "The vector contains: " << std::endl;
+    for (auto const &i : myVector) {
+        std::cout << i << " || ";
+    }
+    std::cout << '\n';
+    const int num{100};
+    std::transform(std::begin(myVector), std::end(myVector),
+                   std::begin(myVector), [&num](int i) { return i + num; });
+    std::cout << "The vector contains: " << std::endl;
+    printContainer(myVector);
 }
