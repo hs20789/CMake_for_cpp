b49c145f8dbd790502c4f4453843202212488218
HeonSu <hs20789@outlook.com>
2025-08-28 21:21:24 +0900

    🧑‍💻: ch21_prac4

--- Project21/solution/ch21_prac4.cpp
+++ Project21/solution/ch21_prac4.cpp
@@ -2,3 +2,24 @@
 #include <iostream>
 #include <regex>
 #include <string>
+
+using namespace std;
+
+int main()
+{
+    regex r{"(?=(?:.*\\d){2})(?=.*[[:lower:]])(?=.*[[:upper:]])(?=.*[[:punct:]]"
+            ").{8,}"};
+    while (true) {
+        cout << "Enter a password (q=quit): ";
+        string str;
+        if (!getline(cin, str) || str == "q") {
+            break;
+        }
+
+        if (regex_match(str, r)) {
+            cout << "  Valid password." << endl;
+        } else {
+            cout << "  Invalid password!" << endl;
+        }
+    }
+}
