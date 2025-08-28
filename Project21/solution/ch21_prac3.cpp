13322664e86d43f8c142025313399ec19a2e2f79
HeonSu <hs20789@outlook.com>
2025-08-28 21:20:22 +0900

    🧑‍💻: ch21_prac3

--- Project21/solution/ch21_prac3.cpp
+++ Project21/solution/ch21_prac3.cpp
@@ -2,3 +2,20 @@
 #include <iostream>
 #include <regex>
 #include <string>
+
+using namespace std;
+
+int main()
+{
+    regex r{"^(.*?)//.*$"};
+    const string replacement{"$1"};
+    while (true) {
+        cout << "Enter a source code snippet, terminated with @:" << endl;
+        string str;
+        if (!getline(cin, str, '@')) {
+            break;
+        }
+
+        cout << regex_replace(str, r, replacement) << endl;
+    }
+}
