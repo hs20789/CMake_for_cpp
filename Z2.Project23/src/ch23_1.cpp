859990e1273c1993e5c719a2b754b343554acf3e
HeonSu <hs20789@outlook.com>
2025-08-30 13:28:19 +0900

    💥: Name Change

---
+++ Z2.Project23/src/ch23_1.cpp
@@ -0,0 +1,17 @@
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+
+int getRandom(int min, int max)
+{
+    return static_cast<int>(rand() % (max + 1UL - min)) + min;
+}
+
+int main()
+{
+
+    srand(static_cast<unsigned int>(time(nullptr)));
+    std::cout << rand() << std::endl;
+
+    std::cout << getRandom(1, 150) << std::endl;
+}
