0316d8e83600f33dac3ebbe4aeda8ef2d92c559a
HeonSu <hs20789@outlook.com>
2025-08-30 11:28:43 +0900

    🧑‍💻: ch22_prac5

---
+++ Project22/solution/ch22_prac5.cpp
@@ -0,0 +1,19 @@
+#include <chrono>
+#include <iostream>
+
+using namespace std;
+using namespace std::chrono;
+
+int main()
+{
+    auto utcTime{sys_days{2025y / August / 30d} + 11h + 35min + 10s};
+
+    zoned_time<seconds> tokyoTime{"Asia/Tokyo", utcTime};
+    zoned_time<seconds> newYorkTime{"America/New_York", tokyoTime};
+    zoned_time<seconds> gmtTime{"GMT", newYorkTime};
+
+    cout << utcTime << endl;
+    cout << gmtTime << endl;
+    cout << tokyoTime << endl;
+    cout << newYorkTime << endl;
+}
