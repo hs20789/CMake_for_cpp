e50a908734a12a00baa12dd479a2a9fb770a5efe
HeonSu <hs20789@outlook.com>
2025-08-30 22:16:38 +0900

    🧑‍💻: ch23_prac2

---
+++ Z2.Project23/solution/ch23_prac2.cpp
@@ -0,0 +1,32 @@
+#include <functional>
+#include <iostream>
+#include <random>
+
+using namespace std;
+
+auto createDiceValueGenerator()
+{
+    random_device seeder;
+    const auto seed{seeder.entropy() ? seeder() : time(nullptr)};
+    ranlux48 engine{static_cast<ranlux48::result_type>(seed)};
+    uniform_int_distribution<int> distribution{1, 6};
+    return bind(distribution, engine);
+}
+
+int main()
+{
+    auto generator{createDiceValueGenerator()};
+
+    while (true) {
+        cout << "Throw two dice? (y or n): ";
+        char answer;
+        cin >> answer;
+        if (answer == 'n' || answer == 'N') {
+            break;
+        }
+
+        auto dieValue1{generator()};
+        auto dieValue2{generator()};
+        cout << format("{} & {}\n", dieValue1, dieValue2);
+    }
+}
